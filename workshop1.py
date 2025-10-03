b45cb38afa23079087d39f810489e7b292d9a5fb
Steen Lindén <35954171+stee0640@users.noreply.github.com>
2025-10-03 12:16:30 +0200

    Update and rename to workshop1.py

---
+++ workshop1.py
@@ -0,0 +1,28 @@
+import pgzrun
+
+WIDTH = 600
+HEIGHT = 600
+bg_y = -600
+
+rumskib = Actor("rumskib1")
+rumskib.bottom = HEIGHT
+rumskib.x = WIDTH//2
+
+def draw():
+    screen.blit("stjerner.png", (0, bg_y))
+    rumskib.draw()
+
+def rumskib_update():
+    if keyboard.left: 
+        rumskib.x -= 5
+    if keyboard.right: 
+        rumskib.x += 5
+
+def update():
+    global bg_y
+    bg_y += 2
+    if bg_y > 0:
+        bg_y -= 1200
+    rumskib_update()
+    
+pgzrun.go()
